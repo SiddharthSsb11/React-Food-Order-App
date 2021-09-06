3f54ac04a0c5d26cecdcf8560f336db3387c16fc
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2021-09-06 23:56:38 +0530

    Card Cmp added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card"
 
 const DUMMY_MEALS = [
   {
@@ -32,7 +33,10 @@ const AvailableMeals = () => {
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>  
+      
     </section>
   );
 };
